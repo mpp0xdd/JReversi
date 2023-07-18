fbb2926524355e151ff78ab3e77563a2f278ded4
mpp <mpp0xdd@gmail.com>
2023-07-18 21:37:51 +0900

    マウスクリックでリバーシ盤に石を配置できるようにする

--- src/jreversi/screen/MainScreen.java
+++ src/jreversi/screen/MainScreen.java
@@ -1,16 +1,20 @@
 package jreversi.screen;
 
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import jglib.component.GameScreen;
 import jreversi.common.IBoard;
+import jreversi.common.Stone;
 import jreversi.component.Board;
 
-public class MainScreen extends GameScreen {
+public class MainScreen extends GameScreen implements MouseListener {
 
   private final IBoard board = new Board();
 
   public MainScreen() {
     setScreenSize(board.width(), board.height());
+    addMouseListener(this);
   }
 
   @Override
@@ -23,4 +27,24 @@ public class MainScreen extends GameScreen {
   protected void runGameLoop() {
     // nop
   }
+
+  @Override
+  public void mouseClicked(MouseEvent e) {
+    int cx = e.getX() / board.squareSize();
+    int cy = e.getY() / board.squareSize();
+    board.put(Stone.BLACK, cx, cy);
+    repaint();
+  }
+
+  @Override
+  public void mousePressed(MouseEvent e) {}
+
+  @Override
+  public void mouseReleased(MouseEvent e) {}
+
+  @Override
+  public void mouseEntered(MouseEvent e) {}
+
+  @Override
+  public void mouseExited(MouseEvent e) {}
 }
