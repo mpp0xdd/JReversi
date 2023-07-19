30f509570cdfbd7067695eca85ab4e4b84199e69
mpp <mpp0xdd@gmail.com>
2023-07-20 00:14:12 +0900

    画面上部に現在のターン（石）の情報を描画する

--- src/jreversi/screen/MainScreen.java
+++ src/jreversi/screen/MainScreen.java
@@ -1,24 +1,31 @@
 package jreversi.screen;
 
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import jglib.component.GameScreen;
+import jglib.util.GameUtilities;
 import jreversi.common.IBoard;
 import jreversi.component.Board;
 
 public class MainScreen extends GameScreen implements MouseListener {
 
-  private final IBoard board = new Board();
+  private final int STATUS_BAR_HEIGHT = 20;
+  private final Font FONT = new Font(Font.SANS_SERIF, Font.BOLD, STATUS_BAR_HEIGHT - 2);
+
+  private final IBoard board = new Board(0, STATUS_BAR_HEIGHT);
 
   public MainScreen() {
-    setScreenSize(board.width(), board.height());
+    setScreenSize(board.width(), board.height() + STATUS_BAR_HEIGHT);
     addMouseListener(this);
   }
 
   @Override
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
+    g.setFont(FONT);
+    GameUtilities.drawString(g, 0, 0, board.currentStone().name());
     board.draw(g);
   }
 
