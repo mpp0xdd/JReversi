eb107018e4d7a030540f9c158a6998ffac20544f
mpp <mpp0xdd@gmail.com>
2023-07-23 10:12:00 +0900

    ボットを削除する

--- src/jreversi/common/IBot.java
+++
@@ -1,7 +0,0 @@
-package jreversi.common;
-
-import java.awt.Point;
-
-public interface IBot {
-  Point compute(IBoard board);
-}
--- src/jreversi/screen/MainScreen.java
+++ src/jreversi/screen/MainScreen.java
@@ -1,24 +1,18 @@
 package jreversi.screen;
 
 import java.awt.Graphics;
-import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import jglib.component.GameScreen;
 import jreversi.common.IBoard;
-import jreversi.common.IBot;
 import jreversi.common.StatusBarBase;
-import jreversi.common.Stone;
 import jreversi.component.Board;
-import jreversi.component.Bot;
 import jreversi.component.StatusBar;
 
 public class MainScreen extends GameScreen implements MouseListener {
 
   private final IBoard board = new Board();
   private final StatusBarBase statusBar = new StatusBar(board);
-  private final IBot bot = new Bot();
-  private final Stone yourStone = Stone.WHITE;
 
   public MainScreen() {
     setScreenSize(board.width(), board.height() + statusBar.height());
@@ -44,10 +38,6 @@ public class MainScreen extends GameScreen implements MouseListener {
     if (!board.asRectangle().contains(e.getPoint())) {
       return;
     }
-    if (board.currentStone() != yourStone) {
-      return;
-    }
-
     int cx = (e.getX() - board.getLocation().x) / board.squareSize();
     int cy = (e.getY() - board.getLocation().y) / board.squareSize();
     board.putStone(cx, cy);
@@ -55,27 +45,14 @@ public class MainScreen extends GameScreen implements MouseListener {
   }
 
   @Override
-  public void mouseReleased(MouseEvent e) {
-    runBot();
-  }
+  public void mouseReleased(MouseEvent e) {}
 
   @Override
-  public void mouseEntered(MouseEvent e) {
-    runBot();
-  }
+  public void mouseEntered(MouseEvent e) {}
 
   @Override
   public void mouseClicked(MouseEvent e) {}
 
   @Override
   public void mouseExited(MouseEvent e) {}
-
-  private void runBot() {
-    if (board.currentStone() == yourStone) {
-      return;
-    }
-    Point p = bot.compute(board);
-    board.putStone(p.x, p.y);
-    repaint();
-  }
 }
