524527cf31a0ff37dc418245e24bab81ab1a9c64
mpp <mpp0xdd@gmail.com>
2023-07-30 11:53:50 +0900

    PointsHolderを削除

--- src/jreversi/component/Bot.java
+++ src/jreversi/component/Bot.java
@@ -3,7 +3,6 @@ package jreversi.component;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Stream;
 import jglib.util.GameUtilities;
 import jreversi.common.BotBase;
@@ -11,32 +10,18 @@ import jreversi.common.IBoard;
 import jreversi.common.ITranscript.IRecord;
 import jreversi.common.Stone;
 
-class PointsHolder {
+public class Bot extends BotBase {
 
   private final List<Point> points;
 
-  public PointsHolder(IBoard board) {
-    Objects.requireNonNull(board);
+  public Bot(IBoard board, Stone stone) {
+    super(board, stone);
     this.points = new ArrayList<>();
     for (int y = 0; y < board.rows(); y++) {
       for (int x = 0; x < board.columns(); x++) {
         this.points.add(new Point(x, y));
       }
     }
-  }
-
-  public Stream<Point> stream() {
-    return this.points.stream();
-  }
-}
-
-public class Bot extends BotBase {
-
-  private PointsHolder holder;
-
-  public Bot(IBoard board, Stone stone) {
-    super(board, stone);
-    this.holder = new PointsHolder(board);
   }
 
   @Override
@@ -63,7 +48,7 @@ public class Bot extends BotBase {
   }
 
   private Stream<Point> pointStream() {
-    return holder.stream();
+    return this.points.stream();
   }
 
   private IRecord getLatestRecord(Point point) {
