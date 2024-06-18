78c4970f8bc039531476fc22a3de060ab1c8be2f
mpp <mpp0xdd@gmail.com>
2024-06-18 22:01:22 +0900

    WIP

--- src/jreversi/JReversi.java
+++ src/jreversi/JReversi.java
@@ -1,12 +1,18 @@
 package jreversi;
 
+import jglib.base.Game;
 import jglib.component.GameScreen;
 import jglib.component.GameWindow;
 import jreversi.screen.MainScreen;
 
-public class JReversi {
+public class JReversi extends Game {
 
   public static void main(String[] args) {
+    launch(JReversi.class);
+  }
+
+  @Override
+  protected void start() {
     GameWindow window = new GameWindow("JReversi");
     GameScreen screen = new MainScreen();
 
